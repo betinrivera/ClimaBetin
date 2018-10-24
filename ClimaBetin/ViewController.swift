fa793e11ef068c5bf54f7e9e7589fd79d12a978a
Alumno <ulsa@Jesus-Corral.local>
2018-10-23 18:40:30 -0700

    Primera version

--- ClimaBetin/ViewController.swift
+++ ClimaBetin/ViewController.swift
@@ -11,23 +11,107 @@ import Alamofire
 
 class ViewController: UIViewController {
 
+    let urlClima = "https://api.openweathermap.org/data/2.5/weather?q=ciudad+obregon&APPID=3326d4de85a204e001116973cc1e557a&units=metric"
+    
+    let urlDolar = "https://free.currencyconverterapi.com/api/v6/convert?q=USD_MXN&compact=y"
     
     @IBOutlet weak var lblGrados: UILabel!
+    @IBOutlet weak var aiCargarClima: UIActivityIndicatorView!
+    
+    @IBOutlet weak var lblPrecioDolar: UILabel!
+    
+    @IBAction func doTapActualizarClima(_ sender: Any) {
+        lblGrados.text = "-"
+        aiCargarClima.startAnimating()
+        Alamofire.request(urlClima).responseJSON { response in
+            if let dictRespuesta = response.result.value as? NSDictionary {
+                
+                if let dictMain = dictRespuesta.value(forKey: "main") as? NSDictionary {
+                    
+                    if let temp = dictMain.value(forKey: "temp") as?
+                        Float {
+                        
+                        self.lblGrados.text = "\(temp)º"
+                        self.aiCargarClima.stopAnimating()
+                        
+                    }
+                    
+                }
+                
+                
+            }
+            else {
+                //Avisarle al usuario que no se pudo leer la respuesta
+            }
+            
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlClima = "http://api.openweathermap.org/data/2.5/weather?q=ciudad+obregon&APPID=3326d4de85a204e001116973cc1e557a&units=metric"
+        aiCargarClima.startAnimating()
         
         
         Alamofire.request(urlClima).responseJSON { response in
-            
-            
-            
+            if let dictRespuesta = response.result.value as? NSDictionary {
+                
+                if let dictMain = dictRespuesta.value(forKey: "main") as? NSDictionary {
+                    
+                    if let temp = dictMain.value(forKey: "temp") as?
+                        Float {
+                        
+                        self.lblGrados.text = "\(temp)"
+                        self.aiCargarClima.stopAnimating()
+                        
+                    }
+                    
+                }
+                
+                
+            }
+            else {
+                //Avisarle al usuario que no se pudo leer la respuesta
+            }
+        }
+        
+        Alamofire.request(urlDolar).responseJSON { response in
+            if let dictRespuesta = response.result.value as? NSDictionary {
+                
+                if let dictCambio = dictRespuesta.value(forKey: "USD_MXN") as? NSDictionary {
+                    
+                    if let val = dictCambio.value(forKey: "val") as?
+                        Double {
+                        
+                        self.lblPrecioDolar.text = "\(val)"
+                        
+                    }
+                    
+                }
+                
+                
+            }
+            else {
+                //Avisarle al usuario que no se pudo leer la respuesta
+            }
         }
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+    
+        
+            
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
